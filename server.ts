002ff8e827ee9786a1a3d2d583909c0ac844d1d6
FMGordillo <facundomgordillo@gmail.com>
2020-10-24 18:22:16 -0300

    :chart_with_upwards_trend: :art: Using EJS with logic Google Tag

--- server.ts
+++ server.ts
@@ -1,12 +1,17 @@
 import path from "path";
 import express, { Response } from "express";
 
+const ejs = require("ejs");
+
 const app = express();
 
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", function onResponse(_, res: Response) {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+  res.render("index", {
+    gmCode: process.env.IS_PROD ? "G-G2F19FBMTQ" : "G-N1SWDDR210",
+  });
 });
 app.get("/privacypolicy.html", function onResponse(_, res: Response) {
   res.sendFile(path.join(__dirname, "..", "public", "privacypolicy.html"));
