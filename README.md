8bb974ad89b889a7b1aeb88077c492b24d3ae5bd
Facundo Martin Gordillo <fmgordillo@pop-os.localdomain>
2020-11-04 23:08:51 -0300

    :construction: Work in progress

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Landing
 
+## Credentials
+
+```
+# Google Analytics:
+
+GA_PRODUCTION=G-G2F19FBMTQ
+GA_TESTING=G-N1SWDDR210
+```
+
 ## TO DO:
 
 - [ ] Tener ingles/español
--- server.ts
+++ server.ts
@@ -1,26 +1,73 @@
 import path from "path";
-import express, { Response } from "express";
-
-const ejs = require("ejs");
+import debug from "debug";
+import express, { Errback, Request, Response, NextFunction } from "express";
 
 const app = express();
 
+const log = debug("favforme_landing-server.ts");
+
+/**
+ * Configuration
+ */
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-app.get("/", function onResponse(_, res: Response) {
-  res.render("index", {
-    gmCode: process.env.IS_PROD ? "G-G2F19FBMTQ" : "G-N1SWDDR210",
-  });
+app.use(function logStuff(req: Request, res: Response, next: NextFunction) {
+  log(`${req.method} ${req.url} - ${res.statusCode}`);
+  next();
 });
-app.get("/privacypolicy.html", function onResponse(_, res: Response) {
+
+const renderProps = (title: string) => ({
+  title,
+  gmCode: process.env.IS_PROD ? "G-G2F19FBMTQ" : "G-N1SWDDR210",
+});
+
+/**
+ * TODO: Create this with files
+ * Routes
+ */
+app.get("/", function handleResponse(_, res: Response) {
+  res.render("index", renderProps("index"));
+});
+app.get("/acciones", function handleResponse(_, res: Response) {
+  res.redirect("/");
+  // TODO: Enable the screen, when mockups are applied
+  // res.render("actions");
+});
+app.get("/acciones/:actionId", function handleResponse(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { actionId } = req.query;
+  if (!actionId) next();
+  // TODO: Enable the screen, when mockups are applied
+  // res.render(`actions/${actionId}`);
+});
+app.get("/privacypolicy.html", function handleResponse(_, res: Response) {
   res.sendFile(path.join(__dirname, "..", "public", "privacypolicy.html"));
 });
-app.get("/terms.html", function onResponse(_, res: Response) {
+app.get("/terms.html", function handleResponse(_, res: Response) {
   res.sendFile(path.join(__dirname, "..", "public", "terms.html"));
 });
-app.get("/register", function onResponse(_, res: Response) {
+app.get("/register", function handleResponse(_, res: Response) {
   res.redirect("https://forms.gle/Jr4FfwZFSTcrHDwaA");
+});
+
+/**
+ * Error handler
+ */
+app.use(function handle404(_, res: Response) {
+  res.status(404).render("notfound");
+});
+app.use(function handleError(
+  err: Errback,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  log("Error", err);
+  res.render("error");
 });
 
 module.exports = app;
