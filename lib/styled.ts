e07b30bcb73cd35a0e65cdda9af525f09899779e
FMGordillo <facundomgordillo@gmail.com>
2020-11-23 23:49:15 -0300

    Added Breakpoints and Spacing

--- lib/styled.ts
+++ lib/styled.ts
@@ -1,9 +1,35 @@
 import baseStyled, { ThemedStyledInterface } from "styled-components";
 
+type Spacing = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+type Breakpoint = "xs" | "sm" | "md" | "lg" | "xl";
+
+const calculateBreakpoint = (key: Breakpoint) => {
+  switch (key) {
+    case "xs":
+      return "0px";
+    case "sm":
+      return "600px";
+    case "md":
+      return "960px";
+    case "lg":
+      return "1280px";
+    case "xl":
+      return "1920px";
+  }
+};
+
 export const theme = {
   color: {
     primary: "#5573f5",
     secondary: "#f38d9f",
+  },
+  spacing: (digit: Spacing) => `${digit * 4}px`,
+  breakpoints: {
+    // "Mobile first"
+    down: (key: Breakpoint) =>
+      `@media (max-width: ${calculateBreakpoint(key)})`,
+    // Not being used rn
+    up: (key: Breakpoint) => `@media (min-width: ${calculateBreakpoint(key)})`,
   },
 };
 
