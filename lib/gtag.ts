920c36fd9d4c346fe29a5881a2529b4b43cb9d23
FMGordillo <facundomgordillo@gmail.com>
2020-11-27 19:51:46 -0300

    :see_no_evil: moar TS errors!?

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,7 +2,9 @@ export const GA_TRACKING_ID = "UA-XXXXXXXXX-X"; // This is your GA Tracking ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
+  // @ts-ignore
   window?.gtag &&
+    // @ts-ignore
     window.gtag("config", GA_TRACKING_ID, {
       page_path: url,
     });
@@ -10,7 +12,9 @@ export const pageview = (url) => {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
+  // @ts-ignore
   window?.gtag &&
+    // @ts-ignore
     window?.gtag("event", action, {
       event_category: category,
       event_label: label,
