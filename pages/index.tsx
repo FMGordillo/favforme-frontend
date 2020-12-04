13c49ae0bb9c154112e0974acb958ef78a876ff9
FMGordillo <facundomgordillo@gmail.com>
2020-12-04 19:45:03 -0300

    :sparkles: Added Contact structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { LayoutComponent as Layout, Modal } from "components";
 import { NextPage } from "next";
+import Image from "next/image";
 import { useState } from "react";
 import styled from "styled-components";
 import { Header } from "../components";
@@ -16,6 +17,13 @@ const Divider = styled.div`
   margin-bottom: 6em;
 `;
 
+const ModalContent = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  align-items: center;
+  justify-items: center;
+`;
+
 const IndexPage: NextPage = () => {
   const [open, setOpen] = useState(false);
 
@@ -23,36 +31,48 @@ const IndexPage: NextPage = () => {
     <Layout>
       {/* TODO: No la pense bien, y no deberia poner props aca */}
       <Modal title="Contacto" onClose={() => setOpen(false)} open={open}>
-        <p>
-          WhatsApp:{" "}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={`https://wa.me/5491123993440?text=${encodeURIComponent(
-              "Vengo del Landing!"
-            )}`}
-          >
-            +5491123993440
-          </a>
-        </p>
-        <p>
-          Llamar:{" "}
-          <a rel="noreferrer" target="_blank" href="tel:+5491123993440">
-            +5491123993440
-          </a>
-        </p>
-        <p>
-          Correo:{" "}
-          <a
-            rel="noreferrer"
-            target="_blank"
-            href={`mailto:hello@favforme.com?subject=${encodeURIComponent(
-              "Contacto"
-            )}`}
-          >
-            hello@favforme.com
-          </a>
-        </p>
+        <ModalContent>
+          <div>
+            <p>
+              WhatsApp:{" "}
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={`https://wa.me/5491123993440?text=${encodeURIComponent(
+                  "Vengo del Landing!"
+                )}`}
+              >
+                +5491123993440
+              </a>
+            </p>
+            <p>
+              Llamar:{" "}
+              <a rel="noreferrer" target="_blank" href="tel:+5491123993440">
+                +5491123993440
+              </a>
+            </p>
+            <p>
+              Correo:{" "}
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href={`mailto:hello@favforme.com?subject=${encodeURIComponent(
+                  "Contacto"
+                )}`}
+              >
+                hello@favforme.com
+              </a>
+            </p>
+          </div>
+          <div>
+            <Image
+              src="/images/contact.png"
+              layout="intrinsic"
+              width={400}
+              height={300}
+            />
+          </div>
+        </ModalContent>
       </Modal>
       <Header
         isIndex
