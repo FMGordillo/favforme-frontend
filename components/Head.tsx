13d2dfe37fd7902061e5f722b78370646e0f5e75
FMGordillo <facundomgordillo@gmail.com>
2020-12-05 15:39:24 -0300

    :eyes: Testing Twitter stuff

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,19 +1,32 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { FunctionComponent } from "react";
 
 export interface HeadProps {
   title?: string;
 }
 
-const HeadComponent: FunctionComponent<HeadProps> = ({ title }) => (
-  <>
-    <Head>
-      <title>{title && `${title} - `}Fundación FavForMe</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="twitter:image" content="/images/plato_de_comida.png" />
-      <link href="/static/assets/styles/fonts.css" rel="stylesheet" />
-    </Head>
-  </>
-);
+const HeadComponent: FunctionComponent<HeadProps> = ({ title }) => {
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>{title && `${title} - `}Fundación FavForMe</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="twitter:image" content="/images/plato_de_comida.png" />
+        <meta
+          name="twitter:card"
+          content="Fundación FavForMe es una ONG que contacta gente que le gusta ayudar con ONGs y empresas con RSE"
+        />
+        <meta name="twitter:site" content="@favforme1" />
+        <meta
+          property="og:url"
+          content={`https://favforme.com${router.asPath}`}
+        />
+        <link href="/static/assets/styles/fonts.css" rel="stylesheet" />
+      </Head>
+    </>
+  );
+};
 
 export { HeadComponent };
