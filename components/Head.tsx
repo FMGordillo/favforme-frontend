5157bb34c5fb3103d3a6a8dabc688cd447b5ee92
FMGordillo <facundomgordillo@gmail.com>
2020-12-05 15:50:38 -0300

    :eyes: Still testing...

--- components/Head.tsx
+++ components/Head.tsx
@@ -13,16 +13,20 @@ const HeadComponent: FunctionComponent<HeadProps> = ({ title }) => {
       <Head>
         <title>{title && `${title} - `}Fundación FavForMe</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="twitter:image" content="/images/plato_de_comida.png" />
         <meta
-          name="twitter:card"
+          property="og:title"
+          content={title && `${title} - ` + "Fundacion FavForMe"}
+        />
+        <meta
+          name="og:description"
           content="Fundación FavForMe es una ONG que contacta gente que le gusta ayudar con ONGs y empresas con RSE"
         />
-        <meta name="twitter:site" content="@favforme1" />
         <meta
           property="og:url"
           content={`https://favforme.com${router.asPath}`}
         />
+        <meta property="og:image" content="/images/plato_de_comida.png" />
+        <meta name="twitter:site" content="@favforme1" />
         <link href="/static/assets/styles/fonts.css" rel="stylesheet" />
       </Head>
     </>
