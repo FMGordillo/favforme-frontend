5026eb7403c24b9424abf2228ce5904e08a7a9bd
Facundo Martin Gordillo <facundomgordillo@gmail.com>
2021-04-30 14:15:21 -0300

    Revert ":shield: Added Sentry (correctly) (#35)"
    This reverts commit 193b8971f9068504059b9f5fc5d5c2ea39389615.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { IndexPage as Index } from "@/containers";
 import { NextPage } from "next";
 import { withAuthUser } from "next-firebase-auth";
 
-const IndexPage: NextPage = () => <Index />;
+const IndexPage: NextPage = () => {
+  return <Index />;
+};
 
 export default withAuthUser()(IndexPage);
