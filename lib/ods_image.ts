00e6b7584f3d457fdb440eec1870f5bd4fb46320
Facundo Martin Gordillo <facundomgordillo@gmail.com>
2021-05-09 09:06:16 -0300

    Hotfix for ODS

--- lib/ods_image.ts
+++ lib/ods_image.ts
@@ -2,7 +2,7 @@ import { ODS } from "./types";
 
 export const getODSImage = (ods: ODS | undefined): string => {
   if (!ods) return "";
-  const odsNumberOriginal = Number(ODS[ods]);
+  const odsNumberOriginal = Number(ODS[ods]) + 1;
   const odsNumber =
     odsNumberOriginal < 10 ? `0${odsNumberOriginal}` : odsNumberOriginal;
   return `/images/ods-images/ods-es-${odsNumber}.png`;
