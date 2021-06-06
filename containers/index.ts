3e377f07f9d8bd79a84284ce498c3c16508c1f71
Facundo Martin Gordillo <facundomgordillo@gmail.com>
2021-06-06 01:57:45 -0300

    :bug: Hotfix for package name

--- containers/index.ts
+++ containers/index.ts
@@ -1,6 +1,6 @@
 export * from "./Actions";
 export * from "./Donate";
-export * from "./Index";
+export * from "./IndexPage";
 export * from "./Nosotros";
 export * from "./Perfil";
 export * from "./DonationsList";
