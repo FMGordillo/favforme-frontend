78f3e4d1ea8efff41d6fc6ff5dc6fd25d08f6d82
Facundo Martin Gordillo <facundomgordillo@gmail.com>
2021-06-19 21:23:11 -0300

    Created adequated containerS

--- containers/Actions/Actions/index.tsx
+++ containers/Actions/Actions/index.tsx
@@ -1,4 +1,4 @@
-import { Action, Container, Layout, Title } from "@/components";
+import { Action, Button, Container, Layout } from "@/components";
 import { ActionContainer, JoinUsContainer, StyledTitle } from "./styles";
 import { ActionI } from "@/lib/types";
 import { NextPage } from "next";
@@ -13,14 +13,12 @@ export const ActionsPage: NextPage<ActionsPageProps> = ({ data }) => {
       <Container>
         <StyledTitle>Acciones Activas</StyledTitle>
         <JoinUsContainer>
-          <Title color="#9f1b32" weight="bold">
-            ODS activos hoy
-          </Title>
           <p style={{ color: "#9f1b32" }}>
             Si sos una ONG es hora de potenciar tu esfuerzo con FavForMe.
             <br />
             Hagamos juntos un lugar mejor para vivir.
           </p>
+          <Button>Sumar mi ONG</Button>
         </JoinUsContainer>
         {data && (data.length || 0) > 0 ? (
           data.map((action) => (
