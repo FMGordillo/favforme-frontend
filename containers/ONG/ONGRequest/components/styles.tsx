e5baaa36df080a519a111bc6dfe76175f4a708a9
Facundo Martin Gordillo <facundomgordillo@gmail.com>
2021-06-21 20:01:47 -0300

    Done

--- containers/ONG/ONGRequest/components/styles.tsx
+++ containers/ONG/ONGRequest/components/styles.tsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
 export const FormFieldContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  width: 100%;
+  padding-bottom: 1em;
 `;
 
 export const BaseField = styled.input`
   border: 1px solid ${({ theme }) => theme.palette.gray.dark};
   border-radius: 16px;
   padding: 4px 8px;
+  max-width: 420px;
+  justify-self: center;
 `;
 
 export const BaseErrorMessage = styled.span`
