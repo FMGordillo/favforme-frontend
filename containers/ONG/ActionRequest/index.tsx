c02a6f58b1c678959abce78825c2557568016fbe
Facundo Martin Gordillo <facundomgordillo@gmail.com>
2021-06-26 08:38:14 -0300

    WIP

--- containers/ONG/ActionRequest/index.tsx
+++ containers/ONG/ActionRequest/index.tsx
@@ -1,22 +1,43 @@
-// import * as Yup from "yup";
-import { Container, Layout } from "@/components";
+import * as Yup from "yup";
+import { Container, FormField, Layout } from "@/components";
+import { Form, Formik } from "formik";
 import { NextPage } from "next";
 
-// interface ActionRequestFormValues {
-//   title?: string;
-// }
+interface ActionRequestFormValues {
+  title: string;
+}
 
-// interface ActionRequestProps {
-//   onSubmit: (values: ActionRequestFormValues) => Promise<void>;
-// }
+interface ActionRequestProps {
+  onSubmit: (values: ActionRequestFormValues) => Promise<void>;
+}
 
-// const ActionRequestSchema = Yup.object();
+const ActionRequestSchema = Yup.object().shape({
+  title: Yup.string().required().label("Nombre de acción").min(5),
+});
 
-export const ActionRequestContainer: NextPage = () => {
+export const ActionRequestContainer: NextPage<ActionRequestProps> = ({
+  onSubmit,
+}) => {
+  const initialValues: ActionRequestFormValues = {
+    title: "",
+  };
+
   return (
     <Layout header title="Sumá tu acción">
       <Container>
         <h1>Hola bb</h1>
+        <Formik
+          onSubmit={onSubmit}
+          initialValues={initialValues}
+          validationSchema={ActionRequestSchema}
+        >
+          {() => (
+            <Form>
+              <FormField name="title" />
+              <button type="submit">Submit :v</button>
+            </Form>
+          )}
+        </Formik>
       </Container>
     </Layout>
   );
