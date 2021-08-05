c4a4365b9f7e68262672534d6740ca7af2fc96a3
FMGordillo <facundomgordillo@gmail.com>
2021-08-05 00:32:47 -0300

    TEST

--- next.config.js
+++ next.config.js
@@ -4,16 +4,6 @@ const moduleExports = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-
-    return config;
-  },
 };
 
 const SentryWebpackPluginOptions = {
