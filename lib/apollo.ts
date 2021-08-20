c6c5f9de818954e62a21e3b3edc3efbdc9d458bf
FMGordillo <facundomgordillo@gmail.com>
2021-08-20 20:34:43 -0300

    :bug::wrench: Rollback to getServerSideProps

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
+  ssrMode: typeof window === "undefined",
   uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
   cache: new InMemoryCache(),
 });
