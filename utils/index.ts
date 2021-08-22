17f2232a9bb44f20eac309661e8ab937f6492de3
FMGordillo <facundomgordillo@gmail.com>
2021-08-22 10:07:25 -0300

    :rocket: Production ready

--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,2 @@
-export const isNotProd = process.env.NEXT_PUBLIC_ENVIRONMENT !== "production";
+// export const isNotProd = process.env.NEXT_PUBLIC_ENVIRONMENT !== "production";
+export const isNotProd = true;
