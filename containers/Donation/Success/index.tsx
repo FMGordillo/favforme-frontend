2ad2e3cc74458c786e80476f29a0ea4936570652
FMGordillo <facundomgordillo@gmail.com>
2021-08-26 01:56:40 -0300

    :sparkles: Added new screen

---
+++ containers/Donation/Success/index.tsx
@@ -0,0 +1,45 @@
+import { Button, FullPageLoading, Layout, Title } from "@/components";
+import { Container } from "./styles";
+import { DonationI } from "@/lib/types";
+import Image from "next/image";
+import { NextPage } from "next";
+import React from "react";
+import router from "next/router";
+
+type DonationSuccessContainerProps = {
+  loading: boolean;
+  donation: DonationI | null;
+};
+
+export const DonationSuccessContainer: NextPage<DonationSuccessContainerProps> = ({
+  loading,
+  donation,
+}) => {
+  return (
+    <Layout header title="Donación exitosa">
+      {loading && <FullPageLoading />}
+      {!loading && (
+        <Container>
+          <Title>¡Felicitaciones!</Title>
+          <p>
+            Se acredit&oacute; tu donaci&oacute;n de{" "}
+            <strong>${donation?.amount}.-</strong>
+            <br />
+            En la acci&oacute;n <strong>{donation?.action?.title}</strong>
+          </p>
+          <Image src="/images/high_five.svg" width={350} height={400} />
+          <p>
+            Gracias a tu ayuda estamos un paso m&aacute;s cerca de alcanzar la
+            <br />
+            meta para que{" "}
+            <strong>
+              {donation?.action?.organization.name} pueda cumplir su objetivo.
+            </strong>
+          </p>
+          <Title>¡Muchas gracias!</Title>
+          <Button onClick={() => router.push("/")}>Volver</Button>
+        </Container>
+      )}
+    </Layout>
+  );
+};
