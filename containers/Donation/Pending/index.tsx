0532cdf2d119b523742aad0230ca4c26c9e9c5d8
FMGordillo <facundomgordillo@gmail.com>
2021-08-30 10:22:34 -0300

    :sparkles: Improved Donation Pending screen

---
+++ containers/Donation/Pending/index.tsx
@@ -0,0 +1,22 @@
+import { Button, Layout, Title } from "@/components";
+import { Container } from "./styles";
+import Image from "next/image";
+import { NextPage } from "next";
+import router from "next/router";
+
+export const DonationPendingContainer: NextPage = () => {
+  return (
+    <Layout header title="Donación pendiente">
+      <Container>
+        <Title>Donación pendiente</Title>
+        <Image src="/images/progress_indicator.svg" width={480} height={400} />
+        <p>
+          Tu operación está siendo procesada,
+          <br />
+          te vamos a mantener informado del estado
+        </p>
+        <Button onClick={() => router.push("/")}>Volver</Button>
+      </Container>
+    </Layout>
+  );
+};
