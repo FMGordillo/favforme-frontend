274db557e60db2d80bca0ae4bb585335a5d24a03
FMGordillo <facundomgordillo@gmail.com>
2021-10-29 19:40:52 -0300

    :sparkles: Added whitepaper

--- containers/IndexPage/components/IntroOne/index.tsx
+++ containers/IndexPage/components/IntroOne/index.tsx
@@ -1,47 +1,58 @@
+import { Button, Text } from "@/components";
 import {
   LeftHandImage,
   Main,
   RightHandImage,
   TextContainer,
   Title,
+  WhitepaperContainer,
 } from "./styles";
 import { FunctionComponent } from "react";
 import Image from "next/image";
-import { Text } from "@/components";
+import Link from "next/link";
 
 const IntroOneSection: FunctionComponent = () => {
   return (
-    <Main>
-      <LeftHandImage>
-        <Image
-          width={350}
-          height={330}
-          layout="fixed"
-          src="/images/mano-01.svg"
-          alt="Mano izquierda"
-        />
-      </LeftHandImage>
-      <TextContainer>
-        <Title>
-          SOMOS LA <strong>PRIMERA</strong> PLATAFORMA DE{" "}
-          <strong>CROWDFUNDING SOCIAL</strong> DE LATINOAMÉRICA
-        </Title>
-        <Text>
-          En FavForMe creamos un espacio que ayuda a las ONGs a conseguir los
-          recursos necesarios para realizar acciones de impacto social a través
-          de crowdfunding
-        </Text>
-      </TextContainer>
-      <RightHandImage>
-        <Image
-          width={340}
-          layout="fixed"
-          height={330}
-          src="/images/mano-02.svg"
-          alt="Mano derecha"
-        />
-      </RightHandImage>
-    </Main>
+    <>
+      <Main>
+        <LeftHandImage>
+          <Image
+            width={350}
+            height={330}
+            layout="fixed"
+            src="/images/mano-01.svg"
+            alt="Mano izquierda"
+          />
+        </LeftHandImage>
+        <TextContainer>
+          <Title>
+            SOMOS LA <strong>PRIMERA</strong> PLATAFORMA DE{" "}
+            <strong>CROWDFUNDING SOCIAL</strong> DE LATINOAMÉRICA
+          </Title>
+          <Text>
+            En FavForMe creamos un espacio que ayuda a las ONGs a conseguir los
+            recursos necesarios para realizar acciones de impacto social a
+            través de crowdfunding
+          </Text>
+        </TextContainer>
+        <RightHandImage>
+          <Image
+            width={340}
+            layout="fixed"
+            height={330}
+            src="/images/mano-02.svg"
+            alt="Mano derecha"
+          />
+        </RightHandImage>
+      </Main>
+      <WhitepaperContainer>
+        <Link passHref href="/whitepaper">
+          <a>
+            <Button textColor="white">Ver Whitepaper</Button>
+          </a>
+        </Link>
+      </WhitepaperContainer>
+    </>
   );
 };
 
