41ec653eacfc560c90d39020e8e95224e8b778ef
FMGordillo <facundomgordillo@gmail.com>
2021-11-10 01:50:46 -0300

    :see_no_evil: Hide login for now + Update snapshots

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,28 +1,28 @@
 import { FunctionComponent, useContext, useState } from "react";
-import { MobileNavigator, Nav, Ul, User } from "./styles";
-import { signIn, useSession } from "next-auth/react";
+import { MobileNavigator, Nav, Ul } from "./styles";
+// import { signIn, useSession } from "next-auth/react";
 import { ContactModal } from "@/components/Modal/components";
 import Image from "next/image";
 import { Logo } from "../assets";
 import { ModalContext } from "@/lib/context";
 import { NavItem } from "./NavItem";
 import { event } from "@/lib/gtag";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 interface NavBarProps {
   isScrolled?: boolean;
 }
 
 const NavBar: FunctionComponent<NavBarProps> = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
+  // const router = useRouter();
+  // const { data: session } = useSession();
   const { handleModal } = useContext(ModalContext);
   const [open, setOpen] = useState(false);
 
-  const handleUserClick = () => {
-    if (session) router.push("/perfil");
-    else signIn();
-  };
+  // const handleUserClick = () => {
+  //   if (session) router.push("/perfil");
+  //   else signIn();
+  // };
 
   const handleMobileOpen = () => {
     setOpen(!open);
@@ -57,14 +57,14 @@ const NavBar: FunctionComponent<NavBarProps> = () => {
         <NavItem href="/#brands">Empresas</NavItem>
         <NavItem onClick={handleContactClick}>Contacto</NavItem>
         {/* <NavItem isProfileButton onClick={() => router.push("/perfil")} /> */}
-        <User onClick={handleUserClick}>
+        {/* <User onClick={handleUserClick}>
           <Image
             src={session?.user?.image ?? "/images/icon_user.svg"}
             width={75}
             height={75}
           />
           <p>{session ? session?.user?.name : "Iniciar sesión"}</p>
-        </User>
+        </User> */}
       </Ul>
       {/* <div style={{ position: "absolute", top: 0, right: 0 }}>Usuario</div> */}
     </Nav>
