a0ab6f980478d7c702ecd20fd0fd04131dbff3eb
FMGordillo <facundomgordillo@gmail.com>
2022-05-16 19:25:59 +0200

    :sparkles: feat(eth): added login with metamask + removed tests

--- components/Layout/Footer/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { Footer } from "./";
-import { render } from "@/utils/testUtils";
-
-describe("[Componentes] Footer", () => {
-  it("renders correctly", async () => {
-    const { asFragment } = render(<Footer />);
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
